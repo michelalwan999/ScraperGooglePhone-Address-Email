ba44312f22dc7491c241cca8499c5ddd005eb31a
michelalwan999 <73309309+michelalwan999@users.noreply.github.com>
2021-08-15 03:40:40 +0300

    Add files via upload

---
+++ Generic - Copy/Scraper/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim scr As New Google_Scraper
+        scr.Scrape()
+
+    End Sub
+End Class
